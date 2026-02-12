b87bc42086e4cacfe6a2261bcaa973721813cfd7
Bao Nguyen <baothai1905@gmail.com>
2026-02-12 14:45:11 +0700

    Init Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Can i finish this in 1 month?
+
+### Tech Stack 
+- Core: Java 21, Spring Boot 4.0.2
+- Database: PostgreSQL + Redis.
+- ORM: Spring Data JPA (Hibernate).
+- Migration: Flyway.
+- Security: Spring Security 6 + JWT.
+- Testing: JUnit 5 + Mockito + Testcontainers.
---
+++ src/main/java/com/barox/ticketflash/TicketFlashApplication.java
@@ -0,0 +1,13 @@
+package com.barox.ticketflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketFlashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketFlashApplication.class, args);
+	}
+
+}
