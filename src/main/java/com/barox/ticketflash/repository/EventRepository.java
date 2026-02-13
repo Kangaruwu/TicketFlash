adb9d7feb4f8e8b47d4ce51cd57262f5e5ce9b65
Bao Nguyen <baothai1905@gmail.com>
2026-02-13 19:37:48 +0700

    fix: override default 'findAll' in EventRepository to solve N+1

--- src/main/java/com/barox/ticketflash/repository/EventRepository.java
+++ src/main/java/com/barox/ticketflash/repository/EventRepository.java
@@ -1,9 +1,18 @@
 package com.barox.ticketflash.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.barox.ticketflash.entity.Event;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    // Tạm thời override để giải quyết N+1 query problem
+    // Vì hàm này để debug tạm
+    // Tương lai sẽ dùng hàm khác với phân trang
+    @Override
+    @EntityGraph(attributePaths = {"venue", "ticketClasses"})
+    public List<Event> findAll();
 }
